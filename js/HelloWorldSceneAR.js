45d74291823efb7ddcdde61d1a0d749e41fa8e4c
Samuel Trew <samuel.trew@outlook.com>
2019-03-10 10:36:57 +0000

    Added poison ivy case

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -13,7 +13,7 @@ import {
   ViroARTrackingTargets,
   ViroSphere,
   ViroSpotLight,
-  ViroQuad,
+  ViroQuad, ViroText,
 } from 'react-viro';
 
 var createReactClass = require('create-react-class');
@@ -39,6 +39,48 @@ var ARCarDemo = createReactClass({
 
         <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')}/>
 
+        <ViroARImageMarker target={"poison"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+
+          <ViroText
+              text = {"Wash your skin!"}
+              scale={[0.5, 0.5, 0.5]}
+              position={[0, 1, -1]} />
+
+          <ViroText
+              text={"Do not scratch!"}
+              scale={[0.5, 0.5, 0.5]}
+              position={[0, .75, -1]} />
+
+          <ViroText
+              text={"Apply calamine or hydrocortisone cream!"}
+              scale={[0.5, 0.5, 0.5]}
+              position={[0, 0.5, -1]} />
+
+          <ViroText
+              text={"Wash all clothing!"}
+              scale={[0.5, 0.5, 0.5]}
+              position={[0, 0, -1]} />
+
+
+          <ViroSpotLight
+              innerAngle={5}
+              outerAngle={25}
+              direction={[0,-1,0]}
+              position={[0, 5, 1]}
+              color="#ffffff"
+              castsShadow={true}
+              shadowMapSize={2048}
+              shadowNearZ={2}
+              shadowFarZ={7}
+              shadowOpacity={.7} />
+
+          <ViroQuad
+              rotation={[-90, 0, 0]}
+              position={[0, -0.001, 0]}
+              width={2.5} height={2.5}
+              arShadowReceiver={true} />
+
+        </ViroARImageMarker>
         <ViroARImageMarker target={"cut"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
           <Viro3DObject
               scale={[0.1, 0.1, 0.1]}
@@ -310,6 +352,11 @@ ViroARTrackingTargets.createTargets({
     source : require('./res/allergic.jpg'),
     orientation : "Up",
     physicalWidth : 0.165
+  },
+  poison : {
+    source : require('./res/poison.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.08
   }
 
 });
